a1d7a3939f344a2a7221a270741dd0598a988670
TheRodrig0 <86856395+TheRodrig0@users.noreply.github.com>
2023-12-08 18:04:17 +0000

    dei uma commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,45 @@
+//CARREGAR O ELEMENTOS
 
+gameScreen = window.document.body.getElementsByClassName('game')[0];
+
+//CARREGAR O ELEMENTOS: Carregar home (pagina inicial)
+
+let loadingHome = [
+    'home', 'textHome'
+];
+
+//CARREGAR O ELEMENTOS: Carregar cenario
+
+let loadingScenery = [
+    'fundo','terreno'
+];
+
+//CARREGAR O ELEMENTOS: Carregar personagem 
+
+let loadingCharacter = [
+    'sonic',
+]
+
+let loadingVectorElements = [loadingScenery, loadingCharacter, loadingHome];
+
+$(document).ready(function LoadingGameElements() {
+
+    for (let i = 0; i < loadingVectorElements.length; i++) {
+
+        let selectedElementVector = loadingElements[i];
+
+        for (let j = 0; j < selectedElementVector.length; j++) {
+
+            const element = document.createElement('div');
+            element.className = selectedElementVector[j];
+            gameScreen.appendChild(element);
+
+            console.log('Elemento ' + selectedElementVector[j] + ' carregado com sucesso. ');
+
+        }
+
+
+    }
+
+
+});
